33699bf972af872d36779430f4d84eada12a78f2
Emiri_Ito <emiri.ito0602@gmail.com>
2022-01-28 00:04:13 +0900

    発展★ー便利なボタン を学習しました。

--- 02_レシピ/01_カウンター/main.js
+++ 02_レシピ/01_カウンター/main.js
@@ -1,8 +1,18 @@
 const display = document.getElementById("display")
 const plusButton = document.getElementById("plus-button")
+const minusButton = document.getElementById("minus-button")
+const multiplyButton = document.getElementById("multiply-button")
 
 let count = 0
 plusButton.onclick = function () {
   count += 1
+  display.textContent = count
+}
+minusButton.onclick = function () {
+  count -= 1
+  display.textContent = count
+}
+multiplyButton.onclick = function () {
+  count *= 2
   display.textContent = count
 }
