3419064620cacf6a7234827fcdd931e103da1a95
Emiri_Ito <emiri.ito0602@gmail.com>
2022-01-28 02:19:14 +0900

    発展★★★ー電卓 を学習しました。

--- 02_レシピ/01_カウンター/main.js
+++ 02_レシピ/01_カウンター/main.js
@@ -16,3 +16,91 @@ multiplyButton.onclick = function () {
   count *= 2
   display.textContent = count
 }
+
+const display1 = document.getElementById("display1")
+const display2 = document.getElementById("display2")
+const zero = document.getElementById("button0")
+const one = document.getElementById("button1")
+const two = document.getElementById("button2")
+const three = document.getElementById("button3")
+const four = document.getElementById("button4")
+const five = document.getElementById("button5")
+const six = document.getElementById("button6")
+const seven = document.getElementById("button7")
+const eight = document.getElementById("button8")
+const nine = document.getElementById("button9")
+const plus = document.getElementById("button+")
+const minus = document.getElementById("button-")
+const multiply = document.getElementById("button×")
+const divide = document.getElementById("button÷")
+
+let keisan1 = 0
+let keisan2 = 0
+
+zero.onclick = function () {
+  keisan2 += 0
+  display2.textContent = keisan2
+}
+one.onclick = function () {
+  keisan2 += 1
+  display2.textContent = keisan2
+}
+two.onclick = function () {
+  keisan2 += 2
+  display2.textContent = keisan2
+}
+three.onclick = function () {
+  keisan2 += 3
+  display2.textContent = keisan2
+}
+four.onclick = function () {
+  keisan2 += 4
+  display2.textContent = keisan2
+}
+five.onclick = function () {
+  keisan2 += 5
+  display2.textContent = keisan2
+}
+six.onclick = function () {
+  keisan2 += 6
+  display2.textContent = keisan2
+}
+seven.onclick = function () {
+  keisan2 += 7
+  display2.textContent = keisan2
+}
+eight.onclick = function () {
+  keisan2 += 8
+  display2.textContent = keisan2
+}
+nine.onclick = function () {
+  keisan2 += 9
+  display2.textContent = keisan2
+}
+
+//足すよ
+plus.onclick = function () {
+  keisan1 = keisan1 + keisan2
+  a()
+}
+//引くよ
+minus.onclick = function () {
+  keisan1 = keisan1 - keisan2
+  a()
+}
+//かけるよ
+multiply.onclick = function () {
+  keisan1 = keisan1 * keisan2
+  a()
+}
+//割るよ
+divide.onclick = function () {
+  keisan1 = keisan1 / keisan2
+  a()
+}
+
+const a = function () {
+  display1.textContent = keisan1
+  keisan2 = 0
+  display2.textContent = keisan2
+}
